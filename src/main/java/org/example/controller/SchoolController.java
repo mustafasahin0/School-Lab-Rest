935c83ceaec0fe8bfa0d3a074a8a25ca21b40317
mustafasahin0 <sahinmustafaenes4@gmail.com>
2024-01-08 18:52:57 -0700

    ResponseWrapper and Students endpoint are completed.

--- src/main/java/org/example/controller/SchoolController.java
+++ src/main/java/org/example/controller/SchoolController.java
@@ -1,7 +1,11 @@
 package org.example.controller;
 
+import org.example.dto.ResponseWrapper;
+import org.example.dto.StudentDTO;
 import org.example.dto.TeacherDTO;
+import org.example.service.StudentService;
 import org.example.service.TeacherService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,13 +16,22 @@ import java.util.List;
 
 public class SchoolController {
     private final TeacherService teacherService;
+    private final StudentService studentService;
 
-    public SchoolController(TeacherService teacherService) {
+    public SchoolController(TeacherService teacherService, StudentService studentService) {
         this.teacherService = teacherService;
+        this.studentService = studentService;
     }
 
     @GetMapping("/teachers")
     public List<TeacherDTO> getAllTeachers() {
         return teacherService.findAll();
+    }
+
+    @GetMapping("/students")
+    public ResponseEntity<ResponseWrapper> getAllStudents() {
+        List<StudentDTO> studentList = studentService.findAll();
+
+        return ResponseEntity.ok(new ResponseWrapper("Students are retrieved successfully", studentList));
     }
 }
