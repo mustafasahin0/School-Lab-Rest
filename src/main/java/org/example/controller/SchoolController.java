fb07d2a538c01df9438f989adf8c4b8bb9825609
mustafasahin0 <sahinmustafaenes4@gmail.com>
2024-01-08 19:39:17 -0700

    Parents endpoint is created.

--- src/main/java/org/example/controller/SchoolController.java
+++ src/main/java/org/example/controller/SchoolController.java
@@ -3,8 +3,10 @@ package org.example.controller;
 import org.example.dto.ResponseWrapper;
 import org.example.dto.StudentDTO;
 import org.example.dto.TeacherDTO;
+import org.example.service.ParentService;
 import org.example.service.StudentService;
 import org.example.service.TeacherService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,10 +19,12 @@ import java.util.List;
 public class SchoolController {
     private final TeacherService teacherService;
     private final StudentService studentService;
+    private final ParentService parentService;
 
-    public SchoolController(TeacherService teacherService, StudentService studentService) {
+    public SchoolController(TeacherService teacherService, StudentService studentService, ParentService parentService) {
         this.teacherService = teacherService;
         this.studentService = studentService;
+        this.parentService = parentService;
     }
 
     @GetMapping("/teachers")
@@ -33,5 +37,16 @@ public class SchoolController {
         List<StudentDTO> studentList = studentService.findAll();
 
         return ResponseEntity.ok(new ResponseWrapper("Students are retrieved successfully", studentList));
+    }
+
+    @GetMapping("/parents")
+    public ResponseEntity<ResponseWrapper> getAllParents() {
+
+        ResponseWrapper responseWrapper = new ResponseWrapper(true, "Parents are successfully retrieved", HttpStatus.ACCEPTED.value(), parentService.findAll() );
+
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .header("Parent","Returned")
+                .body(responseWrapper);
     }
 }
