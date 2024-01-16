eec98c7277cab32d1396d04873ed1645663d4b3c
mustafasahin0 <sahinmustafaenes4@gmail.com>
2024-01-15 18:43:00 -0700

    Added validations.

--- src/main/java/org/example/dto/AddressDTO.java
+++ src/main/java/org/example/dto/AddressDTO.java
@@ -8,6 +8,11 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.example.enums.AddressType;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -19,13 +24,29 @@ public class AddressDTO {
     @JsonIgnore
     private Long id;
 
+    @NotBlank(message = "Street cannot be empty.")
+    @Size(min = 1, max = 100, message = "Street should be between 1 and 100 characters.")
     private String street;
-    private String country;
-    private String state;
-    private String city;
-    private String postalCode;
-    private Integer temperature;
 
+    @NotBlank(message = "Country cannot be empty.")
+    @Size(min = 1, max = 50, message = "Country should be between 1 and 50 characters.")
+    private String country;
+
+    //@NotBlank(message = "State cannot be empty.")
+    @Size(min = 1, max = 50, message = "State should be between 1 and 50 characters.")
+    private String state;
+
+    @NotBlank(message = "City cannot be empty.")
+    @Size(min = 1, max = 50, message = "City should be between 1 and 50 characters.")
+    private String city;
+
+    @NotBlank(message = "Postal code cannot be empty.")
+    @Pattern(regexp = "\\d{5}", message = "Postal code should be 5 digits long.")
+    private String postalCode;
+
+    private String flag;
+
+    @NotNull(message = "Address type cannot be null.")
     private AddressType addressType;
 
     @JsonBackReference(value = "student-address-reference")
@@ -38,5 +59,4 @@ public class AddressDTO {
     private TeacherDTO teacher;
 
     private Integer currentTemperature;
-
 }
